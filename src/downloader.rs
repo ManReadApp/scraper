32e20e632a4523c628a54781083862e601a6e01e
bogie <bogie.jiffy0y@icloud.com>
2024-02-26 22:15:39 +0100

    retry on fail

---
+++ src/downloader.rs
@@ -0,0 +1,18 @@
+use log::warn;
+use reqwest::{Error, RequestBuilder};
+
+pub async fn download(v: RequestBuilder) -> Result<String, Error> {
+    for i in 0..5 {
+        let data = match v.try_clone().unwrap().send().await {
+            Ok(v) => v.text().await,
+            Err(v) => Err(v),
+        };
+        if let Ok(v) = data {
+            return Ok(v);
+        }
+        if i == 4 {
+            return data;
+        }
+    }
+    unreachable!()
+}
